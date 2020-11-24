e2646ae5bc11cf13eb008b0652b9acbb48c81556
superstark02 <superstark02@gmail.com>
2020-11-24 20:12:53 +0530

    11/24/2020

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import video from './Videos/Post.mp4'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video controls width="100%" >
+        <source src={video} type="video/mp4" />
+      </video>
     </div>
   );
 }
