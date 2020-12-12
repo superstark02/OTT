8f642766cc80264cb985f08e55db640783b832c8
superstark02 <superstark02@gmail.com>
2020-12-12 17:39:13 +0530

    12/12/2020

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import SubAppBar from "../Components/SubAppBar"
 import getUser from '../Database/getUser'
 import LogIn from '../Database/logIn'
+import { GoogleLogin } from 'react-google-login';
 
 export class Signin extends Component {
 
     state = {
-        user:null
+        user: null
     }
 
-    componentDidMount(){
-        getUser().then(result=>{
-            this.setState({user:result.name})
+    componentDidMount() {
+        getUser().then(result => {
+            this.setState({ user: result.name })
         })
+    }
+
+    responseGoogle = (response) => {
+        console.log(response);
     }
 
     render() {
@@ -21,14 +26,20 @@ export class Signin extends Component {
                 <SubAppBar />
                 <div className="wrap" style={{ height: "90vh" }} >
                     <div>
-                        <div onClick={()=>{LogIn().then(r=>{console.log(r)})}} >
+                        <div onClick={() => { LogIn().then(r => { console.log(r) }) }} >
                             Google
                         </div>
                         <div>
                             FaceBook
                         </div>
                         <div>
-                            {this.state.user}
+                            <GoogleLogin
+                                clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+                                buttonText="Login"
+                                onSuccess={this.responseGoogle}
+                                onFailure={this.responseGoogle}
+                                cookiePolicy={'single_host_origin'}
+                            />
                         </div>
                     </div>
                 </div>
